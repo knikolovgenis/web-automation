3494166c7636ed02e0559f5b056079cec20d8fa9
knikolov <Konstantinos.Nikolovgenis@Openbet.com>
2022-10-28 19:06:24 +0300

    New selectors added

--- src/pages/EbayHomePage.groovy
+++ src/pages/EbayHomePage.groovy
@@ -14,13 +14,29 @@ class EbayHomePage extends Page{
 		
 		searchInput(wait:true){ $("input.ui-autocomplete-input") }
 		searchBtn(wait:true){ $("#gh-btn")}
-		checkBoxItem() { $("li.x-refine__main__list--value")}
+		modelCheckBoxItems() { $("#x-refine__group_1__0 > ul > li > div > a > div > div > div > .x-refine__multi-select-cbx")}		
+		fakeTabItemsText {$(".fake-tabs > .fake-tabs__items > .fake-tabs__item > a > span").text()}
+		fakeTabItemsBtn {$(".fake-tabs > .fake-tabs__items > .fake-tabs__item")}
+		
 	}
 	
-	def searchItem(item){
-		searchInput.value(item)
-//		assert searchInput.value() == item
-		searchBtn.click()
+		
+	def pickModelCheckBoxItem(def checkBoxString){
+		for (modelCheckBoxItem in modelCheckBoxItems) {
+			if(modelCheckBoxItem.text().contains(checkBoxString)){
+				modelCheckBoxItem.click()
+				break;
+			}
+		}
 	}
+	
+	def selectFakeTabItem(def fakeTabOptionString){
+		for (fakeTabItem in fakeTabItems) {
+			if(fakeTabItem.text().contains(fakeTabOptionString)){
+				fakeTabItem.click()
+				break;
+			}
+		}
+}
 	
 }
